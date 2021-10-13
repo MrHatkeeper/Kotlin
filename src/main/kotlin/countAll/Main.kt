c4a52f75bbf8b3018a1684825c9bcedff31fb216
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2021-10-13 21:47:46 +0200

    CountAll & CountAllNum done

--- src/main/kotlin/countAll/Main.kt
+++ src/main/kotlin/countAll/Main.kt
@@ -4,7 +4,7 @@ fun main() {
     val input = readLine()
     var out = 0
     if (input != null) {
-        for (i in 0..input.length) {
+        for (i in input.indices) {
             if (i % 2 == 0 && input[i].digitToInt() % 3 == 0) out += input[i].digitToInt()
         }
     }
