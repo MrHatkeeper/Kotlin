0377009905cb137755d98fde355a815b71dc7cbf
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2021-10-16 14:04:45 +0200

    ABBA XDDDDDDDDDDDDDDD

--- src/main/kotlin/codeforce550A/Main.kt
+++ src/main/kotlin/codeforce550A/Main.kt
@@ -1,12 +1,28 @@
 package codeforce550A
 
 fun main(){
-    val input = "ABBA"
-    val ab = "AB"
+    val input = "ABA"
+    var ab = false
+    var ba = false
+    var i = 0
 
-    for (i in input.indices){
-        if(input[]){
-
+    while (i < input.length - 1){
+        if(input[i] == 'A' && !ab){
+            if(input[i + 1] == 'B'){
+                i++
+                ab = true
+            }
         }
+        else if(input[i] == 'B' && !ba){
+            if(input[i + 1] == 'A'){
+                i++
+                ba = true
+            }
+        }
+        if(ab && ba){
+            println("YES")
+            break
+        }
+        i++
     }
 }
