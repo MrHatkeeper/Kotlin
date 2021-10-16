46732af708c38359ec68073732d1ea5127f734be
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2021-10-16 14:10:30 +0200

    countAllNum

---
+++ src/main/kotlin/countAllNum/Main.kt
@@ -0,0 +1,14 @@
+package countAllNum
+
+fun main() {
+    val input = readLine()
+    var out = 0
+    if (input != null) {
+        for (i in input.indices) {
+            if (input[i].isDigit()) {
+                out += input[i].digitToInt()
+            }
+        }
+    }
+    println(out)
+}
