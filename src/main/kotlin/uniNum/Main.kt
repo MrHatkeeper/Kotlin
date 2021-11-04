304b0cc1eb9938339d8518ea28a07ae1c52c8d60
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2021-11-04 18:36:13 +0100

    uniNumber

--- src/main/kotlin/uniNum/Main.kt
+++ src/main/kotlin/uniNum/Main.kt
@@ -1,6 +1,19 @@
 package uniNum
 
 fun main(){
-    val list = listOf(readLine()!!.split(","))
+
+    val input = mutableListOf<Int>()
+
+    
+    
+    while (true) {
+        val inpt = readLine()
+        if(inpt.equals("end")) break
+        else input.add(inpt!!.toInt())
+    }
+
+    
+    println(input.distinct().size)
+    println(input.toString())
 
 }
