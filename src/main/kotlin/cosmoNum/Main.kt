8e5708633e912de73150cc6d4b617764b55f343e
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2021-11-10 19:26:15 +0100

    ARMS STRONK NUMBERS

--- src/main/kotlin/cosmoNum/Main.kt
+++ src/main/kotlin/cosmoNum/Main.kt
@@ -1,5 +1,23 @@
 package cosmoNum
 
 fun main(){
+    val input = readLine()!!.toInt()
+
+    for (i in 1..input) {
+        if(moonWalkingNums(i.toString())) println(i)
+    }
+}
+
+fun moonWalkingNums(num: String): Boolean {
+    var output = 0
+    for (i in num.indices){
+        var mezi = 1
+        for (j in num.indices){
+            mezi *= Character.getNumericValue(num[i])
+        }
+        output += mezi
+    }
+
+    return num.toInt() == output
 
 }
