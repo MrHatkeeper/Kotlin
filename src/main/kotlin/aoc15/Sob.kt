6dd08de82dfd0bfcacb577c88d1f8d1bf1266555
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2022-01-15 18:32:02 +0100

    stuff...idk chci umřít

--- src/main/kotlin/aoc15/Sob.kt
+++ src/main/kotlin/aoc15/Sob.kt
@@ -1,32 +1,31 @@
 package aoc15
 
-class Sob {
-    lateinit var name: String
-    var speed: Int = 0
-    var moveTime: Int = 0
-    var restTime: Int = 0
+class Sob (
+    var name: String,
+    var speed: Int,
+    var moveTime: Int,
+    var restTime: Int,
+        ){
+
     var finalDistance: Int = 0
 
-    fun speedPerTime(seconds: Int): Int {
+    fun running(seconds: Int): Int {
         var output = 0
-        var resting = false
-        var restedTime = 0
-        var elapsedTime = moveTime
-        for (i in 0 until seconds) {
-            if (!resting) {
+        var runningTime = moveTime
+        var restingTime = restTime
+
+
+        for(i in 0 until seconds) {
+            if(runningTime > 0){
                 output += speed
-                elapsedTime--
-                if (elapsedTime == 0) {
-                    resting = true
-                }
+                runningTime--
             }
-            if (resting) {
-                if (restedTime == restTime) {
-                    elapsedTime = moveTime
-                    resting = false
-                    restedTime = 0
+            if(runningTime == 0){
+                restingTime--
+                if(restingTime == 0){
+                    runningTime = moveTime
+                    restingTime = restTime
                 }
-                restedTime++
             }
         }
         return output
