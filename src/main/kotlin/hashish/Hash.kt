4ae1a00380ee37d0576527bb8a7e3468059193db
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2022-03-31 22:41:16 +0200

    šéfe já asi nechápu zadání

---
+++ src/main/kotlin/hashish/Hash.kt
@@ -0,0 +1,54 @@
+package hashish
+
+class Hash<T> {
+    var hash = hashSetOf(mutableListOf<T>(), mutableListOf())
+
+
+    fun add(value: T): Boolean {
+        if (!has(value)) {
+            when (value.hashCode() % 2) {
+                0 -> {
+                    if(hash.elementAt(0).add(value)) return true
+                }
+                else -> {
+                    if(hash.elementAt(1).add(value)) return true
+                }
+            }
+        }
+        return false
+    }
+
+    fun has(value: T): Boolean {
+        when (value.hashCode() % 2) {
+            0 -> {
+                if(hash.elementAt(0).contains(value)) return true
+            }
+            else -> {
+                if(hash.elementAt(1).contains(value)) return true
+            }
+        }
+        return false
+    }
+
+    fun delete(value: T): Boolean {
+        if (!has(value)) {
+            when (value.hashCode() % 2) {
+                0 -> {
+                    if(hash.elementAt(0).remove(value)) return true
+                }
+                else -> {
+                    if(hash.elementAt(1).remove(value)) return true
+                }
+            }
+        }
+        return false
+    }
+
+    fun size(): Int {
+        return (hash.elementAt(0).size + hash.elementAt(1).size)
+    }
+
+    fun iterator(value: T) {
+
+    }
+}
---
+++ src/main/kotlin/hashish/Main.kt
@@ -0,0 +1,11 @@
+package hashish
+
+fun main(){
+    var idk = Hash<Int>()
+    var bruh = hashSetOf(5,2,6)
+
+    //idk.add(5)
+    println(bruh.size)
+    println(idk.hash.elementAt(0))
+
+}
