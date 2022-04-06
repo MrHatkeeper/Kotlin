7725a3412ba5ce1e527088bdc5e44bb5bef943a5
MrHatkeeper <karel.krch@educanet.cz>
2022-04-06 10:47:47 +0200

    idk zlomky

--- src/main/kotlin/zlomek/Main.kt
+++ src/main/kotlin/zlomek/Main.kt
@@ -1,5 +1,31 @@
 package zlomek
-
 fun main(){
-    val numOne = Triple(1,111)
+    val numOne = Zlomek("1/2")
+    val numTwo = Zlomek("1/2")
+
+}
+
+
+data class Zlomek(var num: String){
+    var values = arrayListOf(num.split("/")[0].toInt(),num.split("/")[1].toInt())
+    
+    fun plus(firstZlomek: Zlomek, secondZlomek: Zlomek){
+        var gcd = gcd(firstZlomek.values[1], secondZlomek.values[1])
+    }
+
+
+    fun gdc(firstNum: Int, secondNum: Int): Int{
+        var numOne = firstNum
+        var numTwo = secondNum
+
+        while (numOne != numTwo) {
+            if (numOne > numTwo)
+                numOne -= numTwo
+            else
+                numTwo -= numOne
+        }
+
+        return numOne
+    }
+
 }
